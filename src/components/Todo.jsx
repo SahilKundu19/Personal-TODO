57faae2f45d0ceb20c242f309287516024f4cee1
Sahil <kundubabita991@gmail.com>
2024-08-11 13:33:21 +0530

    Personal TODO List App

---
+++ src/components/Todo.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TodoList from './TodoList';
+import FilterButtons from './FilterButtons';
+import { BsSearch, BsPlus } from "react-icons/bs";
+import { addTodo, updateSearchTerm } from '../redux/actions';
+
+const Todo = () => {
+    const todos = useSelector((state) => state.todos);
+    const filter = useSelector((state) => state.filter);
+    const dispatch = useDispatch();
+    const [ newTodoText, setNewTodoText ] = useState('');
+    const [ searchTerm, setSearchTerm ] = useState('');
+
+    const handleAddTodo = (text) => {
+        dispatch(addTodo(text));
+    };
+
+    const handleAddTodoClick = () => {
+        if(newTodoText.trim() !== '') {
+            handleAddTodo(newTodoText.trim());
+            setNewTodoText('');
+        }
+    };
+
+    const handleSearchChange = (value) => {
+        setSearchTerm(value);
+        dispatch(updateSearchTerm(value));
+    };
+
+    return (
+        <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage: `url(https://images.pexels.com/photos/669986/pexels-photo-669986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`}}>
+            <div className="max-w-4xl mx-auto mb-auto sm:mt-8 p-4 border-2 border-blue-500 backdrop-blur-sm bg-white/70 rounded-lg">
+            <h2 className="mt-3 mb-6 text-2xl font-bold text-center text-black uppercase">
+                Personal TODO App
+            </h2>
+            <div className="flex items-center mb-4">
+                <input
+                id="addTodoInput"
+                className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+                type="text"
+                placeholder="Add Todo"
+                value={newTodoText}
+                onChange={(e) => setNewTodoText(e.target.value)}
+                />
+                <button className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none" onClick={handleAddTodoClick}>
+                    <BsPlus size={20}/>
+                </button>
+            </div>
+            <div className="flex flex-col sm:flex-row items-center justify-between gap-4">
+                <FilterButtons />
+                <div className="flex items-center mb-4">
+                    <input
+                    className="flex-grow p-2 border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
+                    type="text"
+                    placeholder="Search Todos"
+                    value={searchTerm}
+                    onChange={(e) => handleSearchChange(e.target.value)}
+                    />
+                    <button className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none">
+                        <BsSearch size={20}/>
+                    </button>
+                </div>
+            </div>
+            <TodoList />
+        </div>
+        </div>
+    );
+};
+
+export default Todo;
